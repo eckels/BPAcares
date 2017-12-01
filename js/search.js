7a0f2d1fef692f6bbf656ed1bedefb8ced50debb
Evan Eckels <eveckels@gmail.com>
2017-11-30 20:16:30 -0600

    everything

---
+++ js/search.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  var searchresults
+  $(document).keyup(function() {
+    searchresults = document.getElementById('search').value;
+    searchresults.toLowerCase();
+    console.log(searchresults);
+
+    var w = 0;
+    while (w < 18) {
+      document.getElementsByClassName('award-link')[w].style.display = "none";
+      w++;
+    }
+
+    var q = 0;
+    while (q < 18) {
+      var match = document.getElementsByClassName('award-link')[q].id;
+      match = match.toLowerCase();
+      console.log(match);
+      console.log(searchresults);
+      if (match.includes(searchresults)) {
+        document.getElementsByClassName('award-link')[q].style.display = "inline-block";
+      }
+      q++;
+    }
+
+    if (searchresults !== "") {
+      var ii = 0;
+      while (ii < 3) {
+        document.getElementsByClassName('wide')[ii].style.display = "none";
+        ii++;
+      }
+    }
+
+    if (searchresults == "") {
+      var i = 0;
+      while (i < 3) {
+        document.getElementsByClassName('wide')[i].style.display = "inline-block";
+        i++;
+      }
+    }
+
+  });
+});
